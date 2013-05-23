7c429ea4272332b5a84102a08080275df9adce90
Heather Miller <heather.miller@epfl.ch>
2013-05-23 16:47:55 +0200

    Making it easier to reuse

---
+++ README.md
@@ -0,0 +1,103 @@
+## uniSync is a super simple uni-dimensional sync (written in Scala)
+
+Why I needed uniSync:
+
+I'm using [Jekyll](http://jekyllrb.com/) with github post-commit hooks to trigger a build of my website on the server, whenever I make a github commit. I wanted my Jekyll-generated site to be automatically merged with what already existed in my `public_html` folder on my web server. It sounds simple, but for some reason it's not. Why?:
+
+- If you make `public_html` your Jekyll target directory, Jekyll will delete it and then regenerate the new site into it. Ouch.
+- If you try to use rsync, you can definitely uni-directionally sync your Jekyll-generated site. But if you delete a bunch of stuff, or move some stuff into a new directory, rsync will just leave it there. For more on why rsync won't work, see the [SO Question](http://unix.stackexchange.com/questions/76739/rsync-delete-files-on-receiving-side-that-were-deleted-on-sending-side-but-do).
+- Newer versions of `unison` have this neat `-nocreation` option which is supposed to prevent files from being created on the destination side. It had issues though- it still somehow bi-directionally synced two directories (meaning it _did_ "create" files on both sides, even though `-nocreation` should stop it.)
+- Rumor has it that [`rdiff-backup` might work](http://unix.stackexchange.com/a/76769/39678). But after spending hours trying to get it to run on a server on which I wasn't an admin, I gave up.
+
+## What it does
+
+If you have a directory structure that looks like this:
+
+     ── A
+        ├── 1.scala
+        ├── 2.scala
+        └── 3.scala
+
+     ── public_html
+        ├── 3.scala
+        ├── yahoo.wee
+        └── yeehaw.scala
+
+
+Let's say that whenever anything new arrives, or gets deleted, in directory `A`, you want those changes to be propagated to directory `public_html`. But we definitely don't want to damage anything else that doesn't exist in `A` but which exists in `public_html`.
+
+Running `uniSync` results in this:
+
+    ── A
+       ├── 1.scala
+       ├── 2.scala
+       └── 3.scala
+
+    ── public_html
+       ├── 1.scala
+       ├── 2.scala
+       ├── 3.scala
+       ├── yahoo.wee
+       └── yeehaw.scala
+
+#### You can delete stuff in the source directory too
+
+Let's say we delete stuff in `A`, like: `2.scala` and `3.scala`. Running `uniSync` will result in this:
+
+    ── A
+       └── 3.scala
+
+    ── public_html
+        ├── 3.scala
+        ├── yahoo.wee
+        └── yeehaw.scala
+
+## You want to use it?
+
+Great. All you need is Java (1.6). Just [download the two jars](), unarchive them to a nice directory somewhere that's on your path, `chmod +x` your stuff, and that's it.
+
+**Always call uniSync from the same directory**. Why? It writes a hidden file, called `.unisync` to the directory you called uniSync from. This file keeps track of what the state of your directory was the last time you called uniSync. It's used to figure out how your directory has changed since the last time you called uniSync (and what files you might've deleted).
+
+**Example setup:**
+
+For example, you might want to unarchive [the bundle]() to `~/bin` on your web server. Then, make sure that unisync is executable: `chmod +x unisync`.
+
+**Example usage:**
+
+    unisync ~/test/a ~/test/b
+
+Before:
+
+    $ tree test/
+    test/
+    ├── a
+    │   ├── 1.txt
+    │   ├── 2.txt
+    │   └── 3.txt
+    └── b
+        ├── 3.txt
+        ├── nono.txt
+        └── yay.txt
+
+After:
+
+    $ tree test/
+    test/
+    ├── a
+    │   ├── 1.txt
+    │   ├── 2.txt
+    │   └── 3.txt
+    └── b
+        ├── 1.txt
+        ├── 2.txt
+        ├── 3.txt
+        ├── nono.txt
+        └── yay.txt
+
+## You want to add stuff to it?
+
+Great. Just make a PR.
+
+And of course: Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---
+++ uniSync.scala
@@ -0,0 +1,123 @@
+#!/bin/sh
+SCRIPT="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
+DIR=`dirname "${SCRIPT}"}`
+exec scala -save -cp $DIR/commons-io-2.4.jar $0 $DIR $SCRIPT $@
+::!#
+
+/** Usage:
+  *  uniSync /path/to/srcDir /path/to/destDir
+  */
+
+import java.io.{File => JFile}
+import java.io.{FileWriter, PrintWriter}
+import scala.io.Source
+import org.apache.commons.io._
+
+object App {
+  def main(args: Array[String]): Unit = {
+
+    val files = args.map(new JFile(_))
+    val currentDir = files(0)
+    val sourceDir = files(2)
+    val destDir = files(3)
+
+    println("You called uniSync inside of directory: " + currentDir)
+    val dotUnisync = new JFile(currentDir, ".unisync");
+    if (!dotUnisync.exists) {
+      println("Creating a new .unisync file in current directory...")
+      initialize(sourceDir, dotUnisync)
+      println("uniSync initialized. Run uniSync again to actually do your uni-directional sync.")
+      return
+    } else println("Reusing .unisync file in current directory...")
+    println("Running uniSync with source dir: %s, and destination dir: %s.\n".format(sourceDir, destDir))
+
+    // figure out the difference between the last run of uniSync and the current run
+    val previousFileList = Source.fromFile(dotUnisync.getAbsolutePath).getLines.toList
+    val currentFileList = mkFileList(sourceDir)
+
+
+    // delete files in destination that have been deleted at the source
+    val deletedFilesAtSrc = previousFileList filter { prevFile =>
+      !currentFileList.exists(file => file.getAbsolutePath == prevFile)
+    }
+
+    val filesToDeleteAtDest = deletedFilesAtSrc map { fileName =>
+      findCorrespondingInDestDir(sourceDir, destDir, new JFile(fileName))
+    }
+
+    if (deletedFilesAtSrc.isEmpty) println("No files have been removed since last run.")
+    else {
+      println(deletedFilesAtSrc.length + " files removed from source directory. Removing the following corresponding files from destination directory...")
+      filesToDeleteAtDest foreach println
+    }
+
+    if (!filesToDeleteAtDest.isEmpty) {
+      print("Deleting... ")
+      filesToDeleteAtDest foreach {
+        file => FileUtils.deleteQuietly(file)
+        val parentDir = file.getParentFile
+        if(parentDir.list().length == 0) FileUtils.deleteQuietly(parentDir) // that means the parent directory is empty
+      }
+      print("DONE.")
+    }
+
+
+    // copy all files from source to destination
+    print("\nCopying to " + destDir + "... ")
+    currentFileList foreach { file =>
+      FileUtils.copyFile(file, findCorrespondingInDestDir(sourceDir, destDir, file))
+    }
+    print("DONE.")
+
+    // update .unisync file with what's now in the source directory
+    print("\nUpdating .unisync... ")
+    writeUnisyncFile(currentFileList.toList, dotUnisync)
+    print("DONE.\n")
+
+    // print summary
+    println("Summary: copied " + currentFileList.length + " files, deleted " + deletedFilesAtSrc.length + " files.")
+
+    // println("\n\nPreviously:")
+    // previousFileList foreach println
+
+    // println("\n\nCurrently:")
+    // currentFileList foreach println
+  }
+
+  def findCorrespondingInDestDir(sourceDir: JFile, destDir: JFile, file: JFile): JFile = {
+    val relativePath = file.getAbsolutePath.drop(sourceDir.getAbsolutePath.length + 1)
+    val destFile = new JFile(destDir, relativePath)
+    destFile
+  }
+
+  def initialize(sourceDir: JFile, dotUnisync: JFile) = {
+    check(sourceDir)
+    val fileList = mkFileList(sourceDir)
+    writeUnisyncFile(fileList.toList, dotUnisync)
+  }
+
+  def writeUnisyncFile(files: List[JFile], dotUnisync: JFile) = {
+    val writer = new PrintWriter(new FileWriter(dotUnisync))
+
+    // output file list to newly-created .unisync file
+    files foreach (file => writer.println(file.getAbsolutePath))
+    writer.close
+  }
+
+  /** Lists all the files contained in `dir` and, recursively, the children of `dir`.
+    * Note that this contains only files, and not directories.
+    */
+  def mkFileList(dir: JFile): Array[JFile] = {
+      val children = if (dir.isDirectory) {
+        dir.listFiles
+      } else Array[JFile]()
+
+      val fileList =
+        if (dir.isDirectory) children.flatMap(child => mkFileList(child))
+        else Array(dir)
+
+      fileList
+  }
+
+  def check(file: JFile): Unit = if (!file.exists) sys.error("Provided path to directory, "+file.getAbsolutePath+" doesn't exist")
+}
